13169d68daa38cd3825d39d7a4939f03d48422bb
Nour Hossam <nourhradwan95@gmail.com>
2024-07-24 14:06:15 +0100

    Test

--- SimpleWedScrapper/Program.cs
+++ SimpleWedScrapper/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+
+        #region Part 1: Builder Pattern
+        //First step is to create two builders that are going to be help us create Search Criteria
+        //and it is going to make it alot more flexable to change the search criteria
+
+        #region Required Data
+        //The data that is required to create a builder is the following
+
+        #endregion
+
+        #endregion
     }
 }
